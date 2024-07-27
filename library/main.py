27443766e07793a92a21c2a3303e64598ba7e34d
ilya_popov <il_pop@mail.ru>
2024-07-27 10:43:35 +0300

    Library.__set_book_2 ->

--- library/main.py
+++ library/main.py
@@ -1,11 +1,67 @@
 """
 - основной файл для работы с библиотекой
 """
+from bookshelf import BookShelf
 
 
-class Main:
-    pass
+class Library:
+    def __init__(self):
+        self.bookshelf = BookShelf()
+
+    def __hello(self):
+        text = "Добрый пожаловать в нашу библиотеку\n" + self.__help()
+        return text
+
+    @staticmethod
+    def __help():
+        text = """
+            1 - help;
+            2 - Добавление книги: Пользователь вводит title, author и year, после чего книга добавляется в библиотеку с уникальным id и статусом “в наличии”;
+            3 - Удаление книги: Пользователь вводит id книги, которую нужно удалить;
+            4 - Поиск книги: Пользователь может искать книги по title, author или year;
+            5 - Отображение всех книг: Приложение выводит список всех книг с их id, title, author, year и status;
+            6 - Изменение статуса книги: Пользователь вводит id книги и новый статус (“в наличии” или “выдана”);
+            При наборе пустого символа программа прекращает работу
+        """
+        return text
+
+    def __set_book_2(self) -> None:
+        """
+        - записываем книгу в библиотеку
+        :return: None
+        """
+        title = input("название книги: ")
+        author = input("автор книги: ")
+        year = input("год издания: ")
+        status = input("статус книги: 'in stock', 'out stock', (необязательный параметр, по умолчанию 'in stock'): ")
+        try:
+            year = int(year)
+        except ValueError:
+            return
+        if bool(status) is False:
+            self.bookshelf.set_book(title=title, author=author, year=year)
+        else:
+            self.bookshelf.set_book(title=title, author=author, year=year, status=status)
+
+    def run(self):
+        print(self.__hello())
+        while True:
+            command_to_execute = input(
+                "Наберите команду (цифра от 1 до 6 или нажмите ввод для окончания работы программы): ")
+            if command_to_execute and command_to_execute not in ("1", "2", "3", "4", "5", "6"):
+                print("Нет такой команды на выполнение")
+                continue
+            if not bool(command_to_execute):
+                print("До свидания")
+                break
+            if command_to_execute == "1":
+                print(self.__help())
+                continue
+            if command_to_execute == "2":
+                self.__set_book_2()
+                continue
 
 
 if __name__ == "__main__":
-    print("Hello World!!!!")
+    library = Library()
+    library.run()
